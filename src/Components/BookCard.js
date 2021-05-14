8b33e29f571c83e928c52d80adcd045eca2826df
orly <orlysan1@gmail.com>
2021-05-14 17:56:22 +0300

    basic bookcard

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import './BookCard.css'
 
 class BookCard extends React.Component{
     constructor(props){
@@ -11,10 +11,10 @@ class BookCard extends React.Component{
     render(){
        
         return(
-            <Card>
-                <Card.Img src="holder.js/100px270" alt="Card image" />
-                <Card.ImgOverlay></Card.ImgOverlay>
-            </Card>
+            <div className="book-card">
+                <img src="https://upload.wikimedia.org/wikipedia/he/e/ed/%D7%97%D7%99%D7%99_%D7%A4%D7%90%D7%99.jpg" alt="Card image"></img>
+            </div>
+            
         )
     }
 }
